357b1d914d5b621cd4ed94be44cac7b9dba1d2f5
LucasDemartini <Lucas.demartini@institutoivoti.com.br>
2017-10-03 20:53:33 -0300

    Implementação dos testes

--- src/br/com/heranca/Teste.java
+++ src/br/com/heranca/Teste.java
@@ -7,8 +7,32 @@ import org.junit.Test;
 public class Teste {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void primeiroteste() {
+		Gerente gerente = new Gerente();
+		gerente.Senha(4444);
+		assertEquals(gerente.autentica(4444), true);
+	}
+
+	@Test
+	public void segundoteste() {
+		Gerente gerente = new Gerente();
+		gerente.Senha(4321);
+		assertEquals(gerente.autentica(4322), false);
+	}
+
+	@Test
+	public void terceiroteste() {
+		Gerente gerente = new Gerente();
+		gerente.Nome("Jonas");
+		assertEquals(gerente.GetNome(), "Jonas");
+	}
+	
+	@Test 
+	public void quartoteste() {
+		Gerente gerente = new Gerente();
+		gerente.Cpf("1122133245");
+		assertEquals(gerente.Getcpf(), "1122133245");
+		
 	}
 
 }
